2560643c8839f9d76b314b9af3378ac6379d72b5
Gerardo Fabián Quispe <gfq0684@gmail.com>
2022-09-25 20:40:34 -0300

    agregamos el archivo js para el button agregar producto

---
+++ js/scripts-producto.js
@@ -0,0 +1,6 @@
+// Para el boton Agregar productos de la pagina productos
+const botonAgregar = document.querySelector(".button__agregar");
+
+botonAgregar.addEventListener("click", e => {
+    location.href = "agregar-producto.html";
+});
