287d663972c9de59c0618e1a7d713b9d815f2675
Gerardo Fabián Quispe <gfq0684@gmail.com>
2022-10-07 10:03:43 -0300

    realizamos algunas modificaciones

--- servicios/productos-servicios.js
+++ servicios/productos-servicios.js
@@ -1,5 +1,26 @@
 const listaProductos = () => fetch("http://localhost:3000/productos").then(respuesta => respuesta.json());
 
+const crearProducto = (name, imageUrl, price, categoria, descripcion) => {
+    return fetch("http://localhost:3000/productos", {
+        method: 'POST',
+        headers: {"Content-Type": "application/json"},
+        body: JSON.stringify({
+            imageUrl,
+            name,
+            price,
+            categoria,
+            descripcion
+        })
+    })
+    .then(respuesta => {
+        if (respuesta.ok) {
+            return respuesta.body;
+        }
+        throw new Error("No fue posible crear un producto.");
+    })
+};
+
 export const productosServicios = {
     listaProductos,
+    crearProducto,
 }
